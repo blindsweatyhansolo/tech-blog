ffc0bbd4f4507b0777eef425c55c3e82a8b7e41a
J Semidey <mtsemidey@gmail.com>
2022-05-20 16:19:07 -0400

    fail-pass test using jest for helper to pluralize 'comment' in templates

--- __tests__/helpers.test.js
+++ __tests__/helpers.test.js
@@ -1,8 +1,17 @@
-const { formatDate } = require('../utils/helpers');
+const { formatDate, formatPlural } = require('../utils/helpers');
 
 // test to re-format created_at date
 test('formatDate() returns a clean date string', () => {
     const date = new Date('2022-05-20 12:12:12');
 
     expect(formatDate(date)).toBe('5/20/2022');
+});
+
+// test to pluralize words (comment)
+test('formatPlural() returns pluralized string', () => {
+    const word1 = formatPlural('Jedi', 1);
+    const word2 = formatPlural('droid', 2);
+
+    expect(word1).toBe('Jedi');
+    expect(word2).toBe('droids');
 });
--- utils/helpers.js
+++ utils/helpers.js
@@ -3,5 +3,13 @@ module.exports = {
     // format date function
     formatDate: date => {
         return `${new Date(date).getMonth() + 1}/${new Date(date).getDate()}/${new Date(date).getFullYear()}`;
+    },
+
+    formatPlural: (word, amount) => {
+        if (amount !== 1) {
+            return `${word}s`;
+        }
+
+        return word;
     }
 };

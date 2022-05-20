eae6bdfd8279610b4cb555c437a448faf57b5c40
J Semidey <mtsemidey@gmail.com>
2022-05-20 14:03:16 -0400

    delete post function created and tested

--- public/javascript/delete-post.js
+++ public/javascript/delete-post.js
@@ -2,7 +2,22 @@
 async function deleteFormHandler(event) {
     event.preventDefault();
 
-    console.log('CLICK!', id);
+    // get id from URL string
+    const id = window.location.toString().split('/')[
+        window.location.toString().split('/').length - 1
+    ];
+
+    // DELETE route using id
+    const response = await fetch(`/api/posts/${id}`, {
+        method: 'DELETE'
+    });
+
+    // on success, redirect to dashboard
+    if (response.ok) {
+        document.location.replace('/dashboard');
+    } else {
+        alert(response.statusText);
+    }
 };
 
 document.querySelector('.delete-btn').addEventListener('click', deleteFormHandler);

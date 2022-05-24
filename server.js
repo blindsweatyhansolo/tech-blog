30aa5b16c1fbad751cf2539b5d98523e106d6188
J Semidey <mtsemidey@gmail.com>
2022-05-24 16:42:14 -0400

    pushing to heroku

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const session = require('express-session');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const sess = {
-    secret: process.env.DB_SECRET,
+    secret: process.env.SECRET,
     cookie: {},
     resave: false,
     saveUninitialized: true,
